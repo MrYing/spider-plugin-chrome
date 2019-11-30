7c3861de16e6f2e68c7f6758d30ee0201bc28032
cokebook <cokebook@outlook.com>
2019-11-30 23:31:03 +0800

    调整包名称

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,15 @@ const rename = require('gulp-rename');
 
 exports.default = function (){
 
- src('src/**/*.js')
+ src('spider/**/*.js')
      .pipe(uglify({
         mangle:true,
         compress:true
      }))
     // 因此使用 gulp-rename 插件修改文件的扩展名
     .pipe(rename({ extname: '.js' }))
-    .pipe( src("src/manifest.json"))
-    .pipe(dest('spider'));
+    .pipe( src("spider/manifest.json"))
+    .pipe(dest('spider-min'));
 
 
 }
